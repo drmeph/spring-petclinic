985e9c9da066f0054d9157fbff9677c4616d7110
kevin.dorfer <kevin.dorfer@ethoca.com>
2018-09-19 20:22:14 -0400

    moved to modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module org.springframework.samples.petclinic {
+    requires java.xml.bind;
+    requires javax.transaction.api;
+    requires java.validation;
+    requires spring.core;
+    requires spring.tx;
+    requires spring.web;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.webmvc;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires org.hibernate.validator;
+    requires cache.api;
+    requires java.persistence;
+}
